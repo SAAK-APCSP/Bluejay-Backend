05a6f578a119aa080dd64f2c4544745a25bd9517
Akhil353 <asingamneni@gmail.com>
2024-02-01 09:29:16 -0800

    Fixing auth_middleware functions

--- auth_middleware.py
+++ auth_middleware.py
@@ -5,37 +5,40 @@ from flask import current_app
 from model.users import User
 
 def token_required(roles=None):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = request.cookies.get("jwt")
-        if not token:
-            return {
-                "message": "Authentication Token is missing!",
-                "data": None,
-                "error": "Unauthorized"
-            }, 401
-        try:
-            data=jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
-            current_user=User.query.filter_by(_uid=data["_uid"]).first()
-            if current_user is None:
+    def decorator(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            token = request.cookies.get("jwt")
+            if not token:
                 return {
-                "message": "Invalid Authentication token!",
-                "data": None,
-                "error": "Unauthorized"
-            }, 401
-            if roles and current_user.role not in roles:
+                    "message": "Authentication Token is missing!",
+                    "data": None,
+                    "error": "Unauthorized"
+                }, 401
+            try:
+                data=jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
+                current_user=User.query.filter_by(_uid=data["_uid"]).first()
+                if current_user is None:
                     return {
-                        "message": "Insufficient permissions. Required roles: {}".format(roles),
-                        "data": None,
-                        "error": "Forbidden"
-                    }, 403
-        except Exception as e:
-            return {
-                "message": "Something went wrong",
-                "data": None,
-                "error": str(e)
-            }, 500
+                    "message": "Invalid Authentication token!",
+                    "data": None,
+                    "error": "Unauthorized"
+                }, 401
+                if roles and current_user.role not in roles:
+                        return {
+                            "message": "Insufficient permissions. Required roles: {}".format(roles),
+                            "data": None,
+                            "error": "Forbidden"
+                        }, 403
+            except Exception as e:
+                return {
+                    "message": "Something went wrong",
+                    "data": None,
+                    "error": str(e)
+                }, 500
 
-        return f(current_user, *args, **kwargs)
+            return f(current_user, *args, **kwargs)
 
-    return decorated
+        return decorated
+
+    return decorator
